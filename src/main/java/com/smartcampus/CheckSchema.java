12de865e1a886679d0fe8f38c8f59da20d3f9961
duttabornil <duttabornil@gmail.com>
2026-05-04 14:15:43 +0530

    made changes to schema

--- src/main/java/com/smartcampus/CheckSchema.java
+++ src/main/java/com/smartcampus/CheckSchema.java
@@ -6,8 +6,8 @@ public class CheckSchema {
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
         try (Connection conn = db.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM Department LIMIT 1")) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM Department LIMIT 1")) {
             ResultSetMetaData meta = rs.getMetaData();
             for (int i = 1; i <= meta.getColumnCount(); i++) {
                 System.out.println(meta.getColumnName(i));
